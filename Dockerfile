eed64afbd5d61240c19f5e3d06446d67099ce433
Joshua Goralczyk <joshgor01@gmail.com>
2021-07-18 22:53:19 -0700

    beta build

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ RUN pip3 install -r requirements.txt
 
 COPY app /usr/python/app
 
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80"]
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8443"]
--- app/api/handlers/upscale.py
+++ app/api/handlers/upscale.py
@@ -9,48 +9,59 @@ env = Env()
 env.read_env()
 
 
+def enhance(buffer):
+    # get the byte array for opencv to read it
+    file_bytes = np.frombuffer(bytearray(buffer), dtype=np.uint8)
+    img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    b, g, r = cv2.split(img)
+    rgb_img = cv2.merge([r, g, b])  # switch it to rgb
+
+    # denoise
+    dst = cv2.fastNlMeansDenoisingColored(rgb_img, None, 0.5, 0.5, 7, 21)
+
+    # convert to PIL
+    im = Image.fromarray(dst)
+
+    # sharpen
+    factor = 1.50
+    enhancer = ImageEnhance.Sharpness(im)
+    im_sharp = enhancer.enhance(factor)
+
+    # contrast
+    factor = 1.0375
+    enhancer = ImageEnhance.Contrast(im_sharp)
+    im_contrast = enhancer.enhance(factor)
+
+    # save to buffer
+    img_byte_arr = BytesIO()
+
+    im_contrast.save(img_byte_arr, format="png", quality=100, subsampling=0)
+
+    return img_byte_arr.getvalue()
+
+
 async def upscale(url: str):
     async with aiohttp.ClientSession() as session:
-        async with session.post(
-            "https://api.deepai.org/api/waifu2x",
-            data={"image": url},
-            headers={"api-key": env("DEEPAI_API_KEY")},
-        ) as r:
-            print("r is done", r)
+        async with session.get(url) as resp:
+            buffer = await resp.read()
+            im = Image.open(buffer)
+            w, h = im.size
 
-            json = await r.json()
-            print("json", json)
-            url = json["output_url"]
+            if w > 400 and h > 600:
+                return enhance(buffer)
 
-            # or use a session you already have
             async with session.get(url) as resp:
                 buffer = await resp.read()
+                async with session.post(
+                    "https://api.deepai.org/api/waifu2x",
+                    data={"image": url},
+                    headers={"api-key": env("DEEPAI_API_KEY")},
+                ) as r:
 
-            # get the byte array for opencv to read it
-            file_bytes = np.frombuffer(bytearray(buffer), dtype=np.uint8)
-            img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-            b, g, r = cv2.split(img)  # get b,g,r
-            rgb_img = cv2.merge([r, g, b])  # switch it to rgb
+                    json = await r.json()
+                    output_url = json["output_url"]
 
-            # # denoise
-            dst = cv2.fastNlMeansDenoisingColored(rgb_img, None, 0.5, 0.5, 7, 21)
+                    async with session.get(output_url) as resp:
+                        buffer = await resp.read()
 
-            # convert to PIL
-            im = Image.fromarray(dst)
-
-            # sharpen
-            factor = 1.50
-            enhancer = ImageEnhance.Sharpness(im)
-            im_sharp = enhancer.enhance(factor)
-
-            # contrast
-            factor = 1.0375
-            enhancer = ImageEnhance.Contrast(im_sharp)
-            im_contrast = enhancer.enhance(factor)
-
-            # save to buffer
-            img_byte_arr = BytesIO()
-
-            im_contrast.save(img_byte_arr, format="png", quality=100, subsampling=0)
-
-            return img_byte_arr.getvalue()
+                        return enhance(buffer)
